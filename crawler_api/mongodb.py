0ea9c3518dfee4ac2921738cb806b0119d33c0cc
sqaz91819 <z390124578@gmail.com>
2017-07-27 11:52:30 +0900

    Mongo module to class
    mongodb.py content to class.
    
    added :
    1. create collection
    2. close connection

--- crawler_api/mongodb.py
+++ crawler_api/mongodb.py
@@ -1,8 +1,7 @@
 from time import time
 from os import path
 from pymongo import MongoClient
-# from pymongo import collection
-from crawler_api import crawler
+from pymongo import collection
 
 
 def read_path(filename):
@@ -11,34 +10,36 @@ def read_path(filename):
     return path
 
 
-def init():
-    server_path = read_path(path.join('crawler_api', 'server_path.txt'))
-    client = MongoClient(server_path, 80)
-    db = client.test
-    return db
+class Mongodb:
+    def __init__(self):
+        self.server_path = read_path(path.join('C:/Users/z3901/Documents/GitHub/MickeyRat/crawler_api', 'server_path.txt'))
+        self.client = MongoClient(self.server_path, 80)
+        self.db = self.client.test
 
+    def create_col(self, c_name):
+        collection.Collection(self.db, c_name, create=True)
 
-def insert_one(doc):
-    db = init()
-    result = db.articles.insert_one(doc)
-    print(result.inserted_id)
+    def insert_one(self, doc):
+        result = self.db.articles.insert_one(doc)
+        print(result.inserted_id)
 
+    def inset_many(self, docs):
+        result = self.db.articles.insert_many([doc for doc in docs if doc is not None])
+        print(result.inserted_ids)
 
-def inset_many(docs):
-    db = init()
-    result = db.articles.insert_many([doc for doc in docs if doc is not None])
-    print(result.inserted_ids)
+    def db_search(self, query):
+        start = time()
+        docs = self.db.articles.find({'title': {'$regex': ".*" + query + ".*"}})
+        print("Query : " + query + " total : " + str(docs.count()))
+        print("Spent time : " + str(time() - start)[0:5] + " secs")
+        return list(docs)
 
+    def db_all(self):
+        start = time()
+        docs = self.db.articles.find({})
+        print("Total : " + str(docs.count()))
+        print("Spent time : " + str(time() - start)[0:5] + " secs")
+        return list(docs)
 
-def db_search(query):
-    start = time()
-    db = init()
-    docs = db.articles.find({'title': {'$regex': ".*" + query + ".*"}})
-    print("Query : " + query + " total : " + str(docs.count()))
-    print(str(time() - start) + "secs")
-    return docs
-# create database
-# collection.Collection(init(), "articles", create=True)
-# test = crawler.json_read("15590.txt")
-# inset_many(test)
-db_search("你的名字")
+    def close(self):
+        self.client.close()
