6d75b19f2fcc89724ebf2f37a7504200bbc10364
andy19960129 <andy19960129@gmail.com>
2017-07-29 19:27:46 +0800

    connect mongodb

--- nlp/GOGOGO.py
+++ nlp/GOGOGO.py
@@ -1,59 +1,64 @@
 from nlp import get_JIEBA
 from crawler_api import crawler
+from crawler_api import mongodb
 import time
 
+codedict = ''
 
 def GOGOGO( start, end ):
     tStart = time.time()  # 計時開始
     ans = []
 
+    #--------------------------------------------------------------
+
+    db = mongodb.Mongodb()
     crawler.download(start, end)
     articles = crawler.json_read(str(start) + str(end) + ".txt")
 
-    get_JIEBA.Frequency_dict_first_process()
-    for a in articles:
-        print(type(a))
-        print(a["url"])
-        jieba_return = get_JIEBA.Get_jieba(a["content"])
-        diction = {
-            'title': a["title"],
-            'url': a["url"],
-            'tag': a["title"][1:4],
-            'text': a["content"],
-            'msegent': jieba_return['word'],
-            'pos': jieba_return['flag']
-        }
-        ans.append(diction)
-
-    get_JIEBA.Frequency_dict_least_process()
-    tEnd = time.time()  # 計時結束
-    print(tEnd - tStart)
-    return ans
-
-def GOGOGO2( start, end ):
-    tStart = time.time()  # 計時開始
-    ans = []
-
-    crawler.download(start, end)
-    articles = crawler.json_read(str(start) + str(end) + ".txt")
+    # --------------------------------------------------------------
 
     get_JIEBA.tf_dict_first_process()
     get_JIEBA.idf_dict_first_process()
     for a in articles:
-        jieba_return = get_JIEBA.Get_jieba_use_tfidf(a["content"])
+        jieba_return = get_JIEBA.Get_jieba(a["content"])
         diction = {
+            'id':a["_id"],
+            'author':a["author"],
+            'label':a["label"],
             'title': a["title"],
             'url': a["url"],
-            'tag': a["title"][1:4],
+            'date_added':a["date_added"],
             'text': a["content"],
             'msegent': jieba_return['word'],
             'pos': jieba_return['flag']
         }
         ans.append(diction)
 
-    get_JIEBA.tfidf_dict_least_process()
     tEnd = time.time()  # 計時結束
     print(tEnd - tStart)
-    return ans
 
-GOGOGO2(1,1)
+    db.inset_many(ans)
+
+
+def dict_least_process(useTFIDF):
+    if useTFIDF :
+        get_JIEBA.tfidf_dict_least_process()
+        codedict = 'tfidf_dict.txt'
+    else :
+        get_JIEBA.Frequency_dict_least_process()
+        codedict = "frequency_dict.txt"
+
+
+def GO100():
+    start = crawler.read_log() + 1
+    end = start+100
+    GOGOGO(start, end)
+
+def GO( num = 10 ):
+    start = crawler.read_log() + 1
+    end = start + num
+    GOGOGO(start, end)
+
+
+
+#GOGOGO(1,1)
--- nlp/get_JIEBA.py
+++ nlp/get_JIEBA.py
@@ -4,12 +4,6 @@ from crawler_api import crawler
 import math
 import jieba.posseg as pseg
 
-#產生"frequency_dict.txt"
-def Frequency_dict_first_process():
-    d = {}
-    d["THE總共"] = 0
-    crawler.json_write("frequency_dict.txt", d)
-
 #產生"tf_dict.txt"
 def tf_dict_first_process():
     d = {}
@@ -22,9 +16,9 @@ def idf_dict_first_process():
     d["THE總共"] = 0
     crawler.json_write("idf_dict.txt", d)
 
-#將"frequency_dict.txt"重新編號，避免數字重複
+#將"tf_dict.txt"重新編號，避免數字重複
 def Frequency_dict_least_process():
-    dict = crawler.json_read("frequency_dict.txt")
+    dict = crawler.json_read("tf_dict.txt")
     sorted_dict = sorted(dict, key=dict.get)
     i = 1
     for w in sorted_dict:
@@ -33,39 +27,8 @@ def Frequency_dict_least_process():
     crawler.json_write("frequency_dict.txt", dict)
 
 
-#結疤分詞，string 為一篇文章內容，使用計出現次數
+#結疤分詞，string 為一篇文章內容
 def Get_jieba( string ) :
-
-    jieba.load_userdict("dict.txt") # 一般辭典
-    jieba.load_userdict("movie_list.txt") # 電影辭典
-
-    pseg_words = pseg.cut(string)
-
-    frequency_dict = crawler.json_read("frequency_dict.txt")
-
-    for one_word in pseg_words : # 計算出現次數
-        if one_word.word in frequency_dict:
-            frequency_dict[one_word.word] += 1
-        else:
-            frequency_dict[one_word.word] = 1
-
-    crawler.json_write("frequency_dict.txt", frequency_dict)
-
-
-    answer = {
-        'word' : [],
-        'flag' : []
-    }
-
-    for w in pseg_words:
-        answer['word'].append(w.word)
-        answer['flag'].append(w.flag)
-
-
-    return answer
-
-#結疤分詞，string 為一篇文章內容，使用TF-IDF
-def Get_jieba_use_tfidf( string ) :
 
     jieba.load_userdict("dict.txt") # 一般辭典
     jieba.load_userdict("movie_list.txt") # 電影辭典
@@ -74,20 +37,19 @@ def Get_jieba_use_tfidf( string ) :
 
     tf_dict = crawler.json_read("tf_dict.txt")
     idf_dict = crawler.json_read("idf_dict.txt")
-    temp_dist = {}
+    temp = []
 
-    for one_word in pseg_words : # 計算出現次數 與 總辭數
-        if one_word.word in tf_dict:
+    for one_word in pseg_words :
+        if one_word.word in tf_dict:    # 計算出現次數 與 總辭數
             tf_dict[one_word.word] += 1
         else :
             tf_dict[one_word.word] = 1
-        if one_word.word in temp_dist:
-            temp_dist[one_word.word] = 1
-        else:
-            temp_dist[one_word.word] = 1
         tf_dict["THE總共"] += 1
+        if one_word.word not in temp:  # 計算出現文章數
+            temp.append(one_word.word)
 
-    for word in temp_dist:
+
+    for word in temp:
         if word in idf_dict:
             idf_dict[word] += 1
         else:
@@ -113,6 +75,7 @@ def Get_jieba_use_tfidf( string ) :
 
 
 
+
 def tfidf_dict_least_process():
     tf_dict = crawler.json_read("tf_dict.txt")
     idf_dict = crawler.json_read("idf_dict.txt")
@@ -132,13 +95,3 @@ def tfidf_dict_least_process():
             tfidf_dict[i] = tf_dict[i] * idf_dict[i]
 
     crawler.json_write("tfidf_dict.txt", tfidf_dict)
-
-
-
-'''''
-t = "昨天很榮幸能去看聲之形 所以一定要來發一下心得 先說結論 我覺得聲之形是部不輸給你的名字的電影"
-print(Get_jieba(t))
-t = "《牆之魘》的背景設定在臺灣光復初期的白色恐怖時代，但是主要探討的不是那個悲慘年代，而是在這種壓抑的時代氛圍下，一個日本共產主義知識"
-print(Get_jieba(t))
-'''''
-
