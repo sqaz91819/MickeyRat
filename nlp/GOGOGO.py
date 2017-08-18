ec43c5bb4462c7c946d7281700b1e162f8318ac1
andy19960129 <andy19960129@gmail.com>
2017-08-19 05:00:09 +0800

    Add files via upload

--- nlp/GOGOGO.py
+++ nlp/GOGOGO.py
@@ -1,136 +1,44 @@
+# coding=utf-8
 from nlp import get_JIEBA
-import time
 from crawler_api import crawler
 from crawler_api import mongodb
 
 
-# 收文章
-def GOToDownload(start, end):
-    tStart = time.time()  # 計時開始
-    ans = []
-
-    # --------------------------------------------------------------
-
-    crawler.download(start, end)
-    articles = crawler.json_read(str(start) + str(end) + ".txt")
-
-    # --------------------------------------------------------------
-
-    get_JIEBA.tf_dict_first_process()
-    get_JIEBA.idf_dict_first_process()
-    for a in articles:
-        jieba_word, jieba_flag = get_JIEBA.Get_jieba(a["content"])
-        diction = {
-            'id':a["_id"],
-            'author':a["author"],
-            'label':a["label"],
-            'title': a["title"],
-            'url': a["url"],
-            'date_added':a["date_added"],
-            'text': a["content"],
-            'segments': jieba_word,
-            'pos': jieba_flag
-        }
-        ans.append(diction)
-
-    tEnd = time.time()  # 計時結束
-    print(tEnd - tStart)
+def interface(search_key: str) -> list:
 
     with mongodb.Mongodb() as db:
-        db.insert_many(ans)
 
+        original_db_data = db.db_all("Articles")
+        jie_ba_db_data = db.db_all("jie_ba_Articles")
 
-# 更新舊有文章，加入分詞結果
-def GOToUpdate():
-    with mongodb.Mongodb() as db:
-        articles = db.db_all()
+        if len(jie_ba_db_data) < len(original_db_data):
+            get_JIEBA.tf_dict_first_process()
+            get_JIEBA.idf_dict_first_process()
+            for i in range(len(jie_ba_db_data), len(original_db_data)):
+                jie_ba_return = get_JIEBA.get_jie_ba(original_db_data[i]["content"])
+                jie_ba_return["title"] = original_db_data[i]["title"]
+                db.insert_one("jie_ba_Articles", jie_ba_return)
+                print("URL : {0}/{1} finished!".format(i, len(original_db_data)))
+            tf_idf_dict_name = get_JIEBA.tf_idf_dict_least_process()
+            tf_idf_dict = crawler.json_read(tf_idf_dict_name)
+        else:
+            tf_idf_dict = db.search_any("record", "the標題", "tf_idf_dict_to" + str(len(jie_ba_db_data)))
 
-    get_JIEBA.tf_dict_first_process()
-    get_JIEBA.idf_dict_first_process()
-    for a in articles:
-        jieba_return = get_JIEBA.Get_jieba(a["content"])
-        with mongodb.Mongodb() as db:
-            db.update_one(a["_id"], jieba_return['word'], jieba_return['flag'])
+        articles_list = db.search_title("Articles", search_key)
+        jie_ba_articles_list = db.search_title("jie_ba_Articles", search_key)
 
+        for a in articles_list:
+            for b in jie_ba_articles_list:
+                if a["title"] == b["title"]:
+                    a["segments"] = b["segments"]
+                    a["pos"] = b["pos"]
+                    break
 
-# 產生encode字典
-def dict_least_process(useTFIDF = True):
-    if useTFIDF :
-        codedict = get_JIEBA.tfidf_dict_least_process()
-    else:
-        codedict = get_JIEBA.Frequency_dict_least_process()
-    return codedict
+        for article in articles_list:
+            encode = []
+            for word in article["segments"]:
+                encode.append(tf_idf_dict[word])
+            article["encoded"] = encode
+            print(article["title"])
 
-
-# 介面
-def InterFace(keyword):
-    with mongodb.Mongodb() as db:
-        ans = db.db_search(keyword)
-    
-    n = dict_least_process()
-    thedict = crawler.json_read(n)
-
-    for article in ans:
-        encode=[]
-        for word in article["segments"]:
-            encode.append(thedict[word])
-        article["encoded"] = encode
-
-    return ans
-
-
-def GO100():
-    start = crawler.read_log() + 1
-    end = start+100
-    GOToDownload(start, end)
-
-
-def GO( num = 10 ):
-    start = crawler.read_log() + 1
-    end = start + num
-    GOToDownload(start, end)
-
-
-def TTTTest(start=1, end=1):
-    ans = []
-
-    # --------------------------------------------------------------
-
-    crawler.download(start, end)
-    articles = crawler.json_read(str(start) + str(end) + ".txt")
-
-    # --------------------------------------------------------------
-
-    get_JIEBA.tf_dict_first_process()
-    get_JIEBA.idf_dict_first_process()
-    for a in articles:
-        jieba_word, jieba_flag = get_JIEBA.Get_jieba(a["content"])
-        diction = {
-            'id': a["_id"],
-            'author': a["author"],
-            'label': a["label"],
-            'title': a["title"],
-            'url': a["url"],
-            'date_added': a["date_added"],
-            'text': a["content"],
-            'segments': jieba_word,
-            'pos': jieba_flag
-        }
-        ans.append(diction)
-        print(a["title"])
-
-    n=dict_least_process()
-    dict = crawler.json_read(n)
-    for article in ans:
-        encode=[]
-        for word in article["segments"]:
-            encode.append(dict[word])
-        article["encoded"] = encode
-        print(article["title"])
-
-    crawler.json_write("test.txt", ans)
-
-
-
-
-# TTTTest(1,10)
+    return articles_list
