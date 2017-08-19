856dfba69a83101957f55d76b87a9e8327e6b1ba
andy19960129 <andy19960129@gmail.com>
2017-08-20 02:52:21 +0800

    YA~

--- nlp/GOGOGO.py
+++ nlp/GOGOGO.py
@@ -4,11 +4,35 @@ from crawler_api import crawler
 from crawler_api import mongodb
 
 
+def go_go_go(num: int)-> None:
+
+    with mongodb.Mongodb() as db:
+
+        original_db_data = db.db_all("articles")
+        jie_ba_db_data = db.db_all("jie_ba_Articles")
+
+        if len(jie_ba_db_data)+num < len(original_db_data):
+            a = len(jie_ba_db_data)+num-1
+        else:
+            a = len(original_db_data)
+
+        get_JIEBA.tf_dict_first_process()
+        get_JIEBA.idf_dict_first_process()
+        for i in range(len(jie_ba_db_data), (len(jie_ba_db_data)+num)):
+            if i < len(original_db_data):
+                jie_ba_return = get_JIEBA.get_jie_ba(original_db_data[i]["content"])
+                jie_ba_return["title"] = original_db_data[i]["title"]
+                db.insert_one("jie_ba_Articles", jie_ba_return)
+                print("{0}/{1} finished!".format(i, a))
+
+    get_JIEBA.up_dict()
+
+
 def interface(search_key: str) -> list:
 
     with mongodb.Mongodb() as db:
 
-        original_db_data = db.db_all("Articles")
+        original_db_data = db.db_all("articles")
         jie_ba_db_data = db.db_all("jie_ba_Articles")
 
         if len(jie_ba_db_data) < len(original_db_data):
@@ -18,13 +42,13 @@ def interface(search_key: str) -> list:
                 jie_ba_return = get_JIEBA.get_jie_ba(original_db_data[i]["content"])
                 jie_ba_return["title"] = original_db_data[i]["title"]
                 db.insert_one("jie_ba_Articles", jie_ba_return)
-                print("URL : {0}/{1} finished!".format(i, len(original_db_data)))
+                print("{0}/{1} finished!".format(i, len(original_db_data)))
             tf_idf_dict_name = get_JIEBA.tf_idf_dict_least_process()
             tf_idf_dict = crawler.json_read(tf_idf_dict_name)
         else:
             tf_idf_dict = db.search_any("record", "the標題", "tf_idf_dict_to" + str(len(jie_ba_db_data)))
 
-        articles_list = db.search_title("Articles", search_key)
+        articles_list = db.search_title("articles", search_key)
         jie_ba_articles_list = db.search_title("jie_ba_Articles", search_key)
 
         for a in articles_list:
@@ -42,3 +66,22 @@ def interface(search_key: str) -> list:
             print(article["title"])
 
     return articles_list
+
+'''
+with mongodb.Mongodb() as db:
+    db.create_col("jie_ba_Articles")
+    db.create_col("record")
+#'''
+
+# interface("模仿遊戲")
+
+# go_go_go(5)
+
+'''
+with mongodb.Mongodb() as db:
+    db.db["jie_ba_Articles"].remove({"as": None})
+    db.db["record"].remove({"as": None})
+    print("!!! record len is " + str(len(db.db_all("record"))))
+    print("!!! jie_ba_Articles len is " + str(len(db.db_all("jie_ba_Articles"))))
+    #print(db.db_all("record")[0]["THE總共"])
+#'''
--- nlp/get_JIEBA.py
+++ nlp/get_JIEBA.py
@@ -4,39 +4,43 @@ from crawler_api import mongodb
 import jieba
 import jieba.posseg as pseg
 import math
-import os
+# import os
 import operator
 
 
 # 產生"tf_dict.txt"
 def tf_dict_first_process()->None:
     with mongodb.Mongodb() as db:
-        d = db.search_any("record", "the標題", "tf_dict")[0]
+        d = db.search_any("record", "the標題", "tf_dict")
         if not d:
             c = {"THE總共": 0, "the標題": "tf_dict"}
             crawler.json_write("tf_dict.txt", c)
         else:
-            crawler.json_write("tf_dict.txt", d)
+            del d[0]['_id']
+            crawler.json_write("tf_dict.txt", d[0])
 
 
 # 產生"idf_dict.txt"
 def idf_dict_first_process()->None:
     with mongodb.Mongodb() as db:
-        d = db.search_any("record", "the標題", "idf_dict")[0]
+        d = db.search_any("record", "the標題", "idf_dict")
         if not d:
             c = {"THE總共": 0, "the標題": "idf_dict"}
             crawler.json_write("idf_dict.txt", c)
         else:
-            crawler.json_write("idf_dict.txt", d)
+            del d[0]['_id']
+            crawler.json_write("idf_dict.txt", d[0])
 
 
 # 計算tf_idf
 def tf_idf_dict_least_process()->str:
-    tf_dict = crawler.json_read(os.path.join('nlp', "tf_dict.txt"))
-    idf_dict = crawler.json_read(os.path.join('nlp', "idf_dict.txt"))
+    tf_dict = crawler.json_read("tf_dict.txt")
+    idf_dict = crawler.json_read("idf_dict.txt")
 
     with mongodb.Mongodb() as db:
         jie_ba_articles_len = len(db.db_all("jie_ba_Articles"))
+        db.db["record"].remove({"the標題": "tf_dict"})
+        db.db["record"].remove({"the標題": "idf_dict"})
         db.insert_one("record", tf_dict)     # !!!!!!!!!!需更新的function
         db.insert_one("record", idf_dict)    # !!!!!!!!!!
 
@@ -91,21 +95,25 @@ def get_jie_ba(string: str)->dict:
     flag = []
 
     for one_word in pseg_words:
-        word.append(one_word.word)
+        if '.' in one_word.word:
+            changed_word = one_word.word.replace('.', '*')
+        else:
+            changed_word = one_word.word
+        word.append(changed_word)
         flag.append(one_word.flag)
-        if one_word.word in tf_dict:    # 計算出現次數 與 總辭數
-            tf_dict[one_word.word] += 1
+        if changed_word in tf_dict:    # 計算出現次數 與 總辭數
+            tf_dict[changed_word] += 1
         else:
-            tf_dict[one_word.word] = 1
+            tf_dict[changed_word] = 1
         tf_dict["THE總共"] += 1
-        if one_word.word not in temp:  # 計算出現文章數
-            temp.append(one_word.word)
+        if changed_word not in temp:  # 計算出現文章數
+            temp.append(changed_word)
 
-    for word in temp:
-        if word in idf_dict:
-            idf_dict[word] += 1
+    for w in temp:
+        if w in idf_dict:
+            idf_dict[w] += 1
         else:
-            idf_dict[word] = 1
+            idf_dict[w] = 1
 
     idf_dict["THE總共"] += 1
 
@@ -117,8 +125,21 @@ def get_jie_ba(string: str)->dict:
     return ans
 
 
+# 上傳tf_dict and idf_dict
+def up_dict()->None:
+    tf_dict = crawler.json_read("tf_dict.txt")
+    idf_dict = crawler.json_read("idf_dict.txt")
+
+    with mongodb.Mongodb() as db:
+        db.db["record"].remove({"the標題": "tf_dict"})
+        db.db["record"].remove({"the標題": "idf_dict"})
+        db.insert_one("record", tf_dict)     # !!!!!!!!!!需更新的function
+        db.insert_one("record", idf_dict)    # !!!!!!!!!!
+
 # Frequency_dict_least_process()
 # tf_idf_dict_least_process()
+
+# up_dict()
 '''
 tf_dict_first_process()
 idf_dict_first_process()
