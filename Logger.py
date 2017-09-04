eceeaae7d6ec154f9acf0522168679cfe8f6a095
Kaltu <lkaxtu@gmail.com>
2017-09-05 00:34:21 +0800

    fix exception

--- Logger.py
+++ Logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def log(frameinfo, *text) -> None:
     try:
         log.verbose
-    except NameError:
+    except (NameError, AttributeError):
         log.verbose = True
     if log.verbose:
         print(datetime.now(), frameinfo.filename, frameinfo.lineno, *text)
