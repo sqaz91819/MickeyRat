94b2e8054c25678694fd28851861fd1b21d57e3b
sqaz91819 <z390124578@gmail.com>
2017-10-23 04:37:21 +0900

    Unhashable type "slice" error
    same to title

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from .models import Question, Choice, Query, ScoreMovie
+from .models import Question, Choice, Query, ScoreMovie, Score
 import sys
+from django.conf import settings
+from copy import copy
+from keras.preprocessing import sequence
 sys.path.insert(0, 'D:\GitHub\MickeyRat')
-
-
 from crawler_api import mongodb
+
+fasttext_model = settings.FAST
 
 
 class IndexView(generic.ListView):
+    fasttext = copy(fasttext_model)
     model = Query
     form_class = Query
     template_name = 'polls/index.html'
@@ -25,10 +29,17 @@ class IndexView(generic.ListView):
         if form.is_valid():
             with mongodb.Mongodb(hash_check=False) as mgd:
                 temp = mgd.search_title('articles', form.query['search'])
+                # ! unhashable type ! nlp = mgd.search_title('jie_ba_Articles', form.query['search'])
+                pos_articles = [t for t in temp if t['score'] > 3]
                 pos = [t['score'] for t in temp if t['score'] > 3]
                 neg = [t['score'] for t in temp if t['score'] == 3]
                 temp = [t['score'] for t in temp if t['score'] > 0]
                 neutral = len(temp) - len(pos) - len(neg)
+
+            if len(pos_articles) > 0:
+                about = True
+            else:
+                about = False
 
             # temp = [1, 2, 3, 4, 5, 1, 1, 4, 4, 4, 4]
             # pos = [t for t in temp if t > 3]
@@ -38,6 +49,8 @@ class IndexView(generic.ListView):
             try:
                 score = (sum(temp) / len(temp)) * 21
                 articles = len(temp)
+                # ! unhashable type ! nlp = sequence.pad_sequences(nlp, maxlen=400)
+                # ! unhashable type ! fasttext_score = int(sum(self.fasttext.predict(nlp, batch_size=32)) / len(temp) * 100)
             except ZeroDivisionError:
                 return render(request, self.template_name, {
                     'form': form,
@@ -47,12 +60,16 @@ class IndexView(generic.ListView):
             return render(request, self.template_name, {
                 'form': form,
                 'search_valid': True,
+                'about': about,
                 'query': form.query['search'],
                 'movie_score': int(score),
                 'articles': articles,
                 'pos': len(pos),
                 'neg': len(neg),
                 'neutral': neutral,
+                'pos_url0': pos_articles[0]['url'],
+                'pos_title0': pos_articles[0]['title'],
+                # ! unhashable type ! 'fast_text': fasttext_score,
             })
 
         return render(request, self.template_name, {
@@ -87,13 +104,35 @@ class ModifyView(generic.ListView):
             score = form.score['score2']
             movie = form.score['movie2']
             with mongodb.Mongodb() as mgd:
-                article = mgd.search_dual('articles', 'title', movie, 'score', int(score))
+                article: dict = mgd.search_dual('articles', 'title', movie, 'score', int(score))
             try:
+                article['content'] = article['content'].replace(' ', '\n')
+                article['id0'] = article['_id']
                 return render(request, self.template_name, article)
             except IndexError:
                 return render(request, 'polls/labelfix.html', {'error_message': '資料庫無此電影或沒有該範圍分數的文章'})
+            except TypeError:
+                return render(request, 'polls/labelfix.html', {'error_message': '資料庫無此電影或沒有該範圍分數的文章'})
 
         return render(request, 'polls/labelfix.html', {'error_message': 'Not valid input.'})
+
+
+def score(request):
+    if request.method == 'POST':
+        form = Score(request.POST)
+
+        if form.is_valid():
+            id1 = form.id1['id1']
+            get_score = form.id1['score1']
+
+            with mongodb.Mongodb() as mgd:
+                mgd.update_score('articles', id1, int(get_score))
+                # mgd.update_score('jie_ba_Articles', _id, get_score)
+                mgd.update_modified(id1)
+
+            return render(request, 'polls/labelfix.html')
+
+        return render(request, 'polls/index.html', {'form': form, })
 
 
 def vote(request, question_id):
